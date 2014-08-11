da5d1092435d95fc89e7471767ba625ccab86b49
Sunil Unka <sunil.unka@gmail.com>
2014-08-11 17:38:32 +0000

    Initial commit of bet js program.

---
+++ application.js
@@ -0,0 +1,11 @@
+
+//;(function( window, document){
+
+  var getBet = function(){
+    betField = document.getElementById("bet");
+    betAmount = betField.value;
+    console.log(betAmount);
+  };
+
+
+//}( window, document));
